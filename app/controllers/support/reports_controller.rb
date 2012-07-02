6c5a5afb5fda74cabc1a5ef6b8f16cd7027cd3ba
Tom Caspy <tom@sir.co.il>
2012-07-02 05:14:05 +0300

    adding reports controller, which is currently just a stub

---
+++ app/controllers/support/reports_controller.rb
@@ -0,0 +1,4 @@
+class Support::ReportsController < ApplicationController
+  # currently, this controller is a stub, waiting for the reporter gem
+  # git@github.com:unorthodoxgeek/reporter.git
+end
---
+++ app/helpers/support/reports_helper.rb
@@ -0,0 +1,2 @@
+module Support::ReportsHelper
+end
